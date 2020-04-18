f4b85ad3a16060a75b7a9c2112326c09aa970e05
Prosper Ekwerike <43956851+Pekwerike@users.noreply.github.com>
2020-04-18 01:41:34 +0100

    Add files via upload

---
+++ StackDs_LinkedListImplementation/src/LinkedList.java
@@ -0,0 +1,62 @@
+public class LinkedList {
+
+    /**
+     * initializing the First node in the linkedList
+     **/
+    Node first = null;
+
+    /**
+     * Data class to represent a single node in the linkedList
+     **/
+    class Node {
+        String Item;
+        Node next;
+    }
+
+    /**
+     * Function to check if the list is empty
+     **/
+    public Boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * Putting items inside the linkedList
+     **/
+    public void push(String dataToInsert) {
+        /**Check if the linked list is empty**/
+        if (isEmpty()) {
+            first = new Node();
+            first.Item = dataToInsert;
+            first.next = null;
+        }
+        /**If list is not empty**/
+        else {
+            /**Hold a link to the list**/
+            Node oldList = first;
+            /**Create a new node to be added on the top of linked list**/
+            first = new Node();
+            first.Item = dataToInsert;
+            /**Make the new node point back to the rest of the list**/
+            first.next = oldList;
+        }
+    }
+
+    /**
+     * Returning items from the list
+     **/
+    public String pop() {
+        String itemToBePoped;
+        /**Check if the linked list is empty**/
+        if (isEmpty()) {
+            return null;
+        } else {
+            /**Retrieving the item in the top node**/
+            itemToBePoped = first.Item;
+            /**Making the next node on the list the top node**/
+            first = first.next;
+            return itemToBePoped;
+        }
+
+    }
+}
---
+++ StackDs_LinkedListImplementation/src/MainClass.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class MainClass {
+
+    public static void main(String args[]){
+        File myTestFile = new File("C:\\Users\\Prosper's PC\\Documents\\LinkedListTesting.txt");
+        LinkedList myLink = new LinkedList();
+        try {
+            Scanner readInput = new Scanner(myTestFile);
+            while (readInput.hasNextLine()){
+                String data = readInput.nextLine();
+                if(data.equals("-")){
+                    System.out.println(myLink.pop());
+                }
+                else {
+                    myLink.push(data);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
